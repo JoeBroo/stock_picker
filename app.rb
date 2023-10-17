8afa74b78c5266af1f039592064dd24c47a8fe34
JoeBroo <jdoe40630@gmail.com>
2023-10-16 21:05:28 -0400

    Remove minmax method and find each individually

--- app.rb
+++ app.rb
@@ -4,19 +4,37 @@ def stock_picker(prices)
 
     puts "How much money do you want to spend?"
     money = gets.chomp.to_i
-    "You can spend $#{money}"
+    puts "You can spend $#{money}"
 
-    prices.each { |price| puts price }
+    #Takes total money and subtracts the total value of stock prices
+    prices.each do |purchase|
+        money = money - purchase
+    end
+
+    p "You have $#{money} remaining"
 
     #Keeps track of best day to buy and best day to sell stock
-    days = []
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
-    #Store lowest and highest prices
-    lowest_highest = prices.minmax
-    puts "The lowest and highest stock prices are: #{lowest_highest}"
+    days.each_with_index {|day, idx| puts "#{day} is day #{idx + 1} of the week"}
+
+
+    #Find cheapest stock 
+    cheap = prices.min
+    puts cheap
+
+    #Find its location
+    cheap_loc = prices.find_index {|e| e == cheap }
+
+    #Find most expensive stock
+    expensive = prices.max
+    puts expensive
+
+    #Find its location
+    expensive_loc = prices.find_index {|e| e == expensive }
+
 
 
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
-    
