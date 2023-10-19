ad5d055270527472d505c9de9a2459a2b18a7fe5
JoeBroo <jdoe40630@gmail.com>
2023-10-18 22:57:36 -0400

    Find indices of cheap and expensive stock prices

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ def stock_picker(prices)
     puts "Welcome to the Stock Picker!"
 
     puts "How much money do you want to spend?"
+
     money = gets.chomp.to_i
+
     puts "You can spend $#{money}"
 
     #Takes total money and subtracts the total value of stock prices
     prices.each do |purchase|
+
         money = money - purchase
+
     end
 
     p "You have $#{money} remaining"
@@ -16,24 +20,30 @@ def stock_picker(prices)
     #Keeps track of best day to buy and best day to sell stock
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+    #List days of the week
     days.each_with_index {|day, idx| puts "#{day} is day #{idx + 1} of the week"}
 
 
-    #Find cheapest stock 
+    #Find cheapest price
     cheap = prices.min
-    puts cheap
 
-    #Find its location
-    cheap_loc = prices.find_index {|e| e == cheap }
-
-    #Find most expensive stock
+    #Find most expensive price
     expensive = prices.max
-    puts expensive
 
-    #Find its location
-    expensive_loc = prices.find_index {|e| e == expensive }
+    #Find their locations
+    locations = []
 
+    prices.each_with_index do |price, idx|
 
+        if price == cheap || price == expensive
+
+            locations.push(idx)
+
+        end
+        
+    end
+
+    p locations
 
 end
 
